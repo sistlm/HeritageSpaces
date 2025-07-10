c3f96e7c7975fbf1ca10c41c030e1f725f543d51
sistlm <sistladurgamahenderrao@gmail.com>
2025-07-11 01:29:12 +0530

    add

---
+++ Pages/Gallery.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeritageSpaces.Pages
+{
+    public class GalleryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Shared/About.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeritageSpaces.Pages.Shared
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
